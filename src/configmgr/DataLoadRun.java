39f853c8cca9aa120f0f5ed7a468afcc3530d92b
Varun Khurana <varunkhurana2904@gmail.com>
2013-11-13 04:30:49 -0500

    Database queries file added

--- src/configmgr/DataLoadRun.java
+++ src/configmgr/DataLoadRun.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class DataLoadRun {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
 		Connection con;
 		Statement stmt;
 		ResultSet res_set;
