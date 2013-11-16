b26587cc34502948a6569fb98b2e8e8d010a4cc6
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-11-15 19:45:50 -0500

    Updated classpath and minor changes

--- src/configmgr/DataLoadRun.java
+++ src/configmgr/DataLoadRun.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class DataLoadRun {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello World");
+		// System.out.println("Hello World");
 		Connection con;
 		Statement stmt;
 		ResultSet res_set;
@@ -19,7 +19,7 @@ public class DataLoadRun {
 		String server = "ora.csc.ncsu.edu";
 		String db_name = "ORCL";
 		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@"+server+":"+port+":"+db_name;
+		String url = "jdbc:oracle:thin:@" + server + ":" + port + ":" + db_name;
 
 		try {
 			Class.forName(driver);
@@ -28,18 +28,17 @@ public class DataLoadRun {
 			stmt.executeQuery("Insert into varun values (5000,'1085 khatiwala tank')");
 			stmt = con.createStatement();
 			res_set = stmt.executeQuery("select * from varun");
-			while(res_set.next())
-			{
+			while (res_set.next()) {
 				System.out.println(res_set.getInt("ssn"));
 				System.out.println(res_set.getString(2));
-				
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}		
-		
+		}
+
 	}
 
 }
